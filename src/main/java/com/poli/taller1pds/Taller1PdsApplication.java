d859f497ae647262a1641ca4fb8c20384b595aad
CarlosMatiz2 <FalcoMaster.-22>
2022-10-16 16:05:47 -0500

    First commit

---
+++ src/main/java/com/poli/taller1pds/Taller1PdsApplication.java
@@ -0,0 +1,13 @@
+package com.poli.taller1pds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller1PdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Taller1PdsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poli/taller1pds/Taller1PdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poli.taller1pds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller1PdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
